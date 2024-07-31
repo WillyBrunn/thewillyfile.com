a9f6d7af10d88fd9963a8ea0caf099299001a930
WillyBrunn <119013292+WillyBrunn@users.noreply.github.com>
2024-07-31 19:50:19 +0100

    Updated button behaviour in album page

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ coll[i].addEventListener("click", function() {
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
     content.style.display = "none";
+    this.classList.remove("hover-effect")
     this.parentNode.style.background = "";
     } else {
     content.style.display = "block";
-    if (this.parentNode.classList.contains("Pride")) {
-      this.parentNode.style.background = "linear-gradient(135deg, rgb(0, 0, 0), rgb(54, 35, 18), rgb(120, 79, 23), rgb(181, 63, 27), rgb(237, 34, 36), rgb(243, 91, 34), rgb(249, 150, 33), rgb(245, 193, 30), rgb(241, 235, 27) 48%, rgb(241, 235, 27), rgb(241, 235, 27) 52%, rgb(99, 199, 32), rgb(12, 155, 73), rgb(33, 135, 141), rgb(57, 84, 165), rgb(97, 55, 155), rgb(147, 40, 142))";
-    }
+      if (this.parentNode.classList.contains("Pride")) {
+        this.parentNode.style.background = "linear-gradient(135deg, rgb(0, 0, 0), rgb(54, 35, 18), rgb(120, 79, 23), rgb(181, 63, 27), rgb(237, 34, 36), rgb(243, 91, 34), rgb(249, 150, 33), rgb(245, 193, 30), rgb(241, 235, 27) 48%, rgb(241, 235, 27), rgb(241, 235, 27) 52%, rgb(99, 199, 32), rgb(12, 155, 73), rgb(33, 135, 141), rgb(57, 84, 165), rgb(97, 55, 155), rgb(147, 40, 142))";
+      }
+    this.classList.add("hover-effect");
     }
 });
 }
